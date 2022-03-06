50244e5c211ffad13f08debe9dc01ff2a5195961
neeryks <akashdeep.singh49@gmial.com>
2022-03-06 19:54:13 +0530

    Adding Features

--- startapp.py
+++ startapp.py
@@ -1,11 +1,25 @@
+from pydoc import splitdoc
 import pandas as pd
 
 
 ## MAking Models For Chatbot ##
+dico = ['hello','bye']
 def cbot():
     print("Hi I am Cbot. How Can i help You")
     b = input()
+    #splitlist = list(b.split(" ")
     for d in dico:
-        if d == b:
+        splitlist = list(b.split(" "))
+        for sp in splitlist:
+            if d == sp:
+                print("Ok let me check that for you! Hold Tight")
+
+cbot()
+
+
+
+
+
+
 
             
