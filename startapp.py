9b18df299ce86c2dd5a7adeb8733c432cd9e2f2c
neeryks <akashdeep.singh49@gmial.com>
2022-03-06 18:50:51 +0530

    update

--- startapp.py
+++ startapp.py
@@ -2,3 +2,10 @@ import pandas as pd
 
 
 ## MAking Models For Chatbot ##
+def cbot():
+    print("Hi I am Cbot. How Can i help You")
+    b = input()
+    for d in dico:
+        if d == b:
+
+            
